b3f7edd2f2240396e095cd6e116ebbfa0dce4e37
SaraVieira <hey@iamsaravieira.com>
2026-08-29 18:20:23 +0100

    add auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,21 @@
-import { betterAuth } from 'better-auth'
-import { tanstackStartCookies } from 'better-auth/tanstack-start'
+import { env } from "#/env";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { db } from "#/db"; // your drizzle instance
 
 export const auth = betterAuth({
+  database: drizzleAdapter(db, { provider: "sqlite" }),
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    github: {
+      enabled: true,
+      clientId: env.GITHUB_CLIENT_ID!,
+      clientSecret: env.GITHUB_CLIENT_SECRET!,
+    },
+  },
+
   plugins: [tanstackStartCookies()],
-})
+});
